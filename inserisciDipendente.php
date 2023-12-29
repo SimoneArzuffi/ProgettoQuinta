cd6d66958c46664fe51e44e1fbedcb8a9363d77f
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2023-12-29 10:32:36 +0000

    primo css

--- inserisciDipendente.php
+++ inserisciDipendente.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<?php
+    <?php
         session_start();
         if(isset($_POST["nome"]) && isset($_POST["cognome"]) && isset($_POST["data_di_nascita"]) && isset($_POST["cf"])){
             $_SESSION['POST'] = $_POST;
@@ -8,26 +8,76 @@
         }
     ?>
     <head>
-        <title>
-            inserisci dipendente
-        </title>
+        <title>Inserisci Dipendente</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #f4f4f4;
+            }
+
+            form {
+                width: 300px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+
+            p {
+                margin-top: 10px;
+                margin-bottom: 5px;
+            }
+
+            input[type="text"],
+            input[type="date"],
+            input[type="submit"] {
+                width: calc(100% - 10px);
+                padding: 10px;
+                margin-bottom: 15px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+
+            input[type="submit"] {
+                background-color: #4caf50;
+                color: #fff;
+                cursor: pointer;
+            }
+
+            input[type="submit"]:hover {
+                background-color: #0056b3;
+            }
+
+            a {
+                display: block;
+                margin-top: 10px;
+                text-align: center;
+                text-decoration: none;
+                color: #007bff;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
     </head>
-
-
     <body>
-        <!-- inserisci la possibilita di inserire un nuovo dipendente -->
         <form action="inserisciDipendente.php" method="POST">
-            <p>inserisci il nome del dipendente</p>
-            <input type="text" name="nome" placeholder="nome" required><br><br>
-            <p>inserisci il cognome del dipendente</p>
-            <input type="text" name="cognome" placeholder="cognome" required><br><br>
-            <p>inserisci il codice fiscale del dipendente</p>
-            <input type="text" name="cf" placeholder="codice fiscale" style="text-transform: uppercase;" required><br><br>
-            <p>inserisci la data di nascita del dipendente</p>
-            <input type="date" name="data_di_nascita" placeholder="data di nascita" required><br><br>
-            <input type="submit" value="inserisci dipendente">
-        </form><br><br>
-        <a href="home.php">torna alla home</a>
-
+            <p>Inserisci il nome del dipendente</p>
+            <input type="text" name="nome" placeholder="Nome" required><br>
+            <p>Inserisci il cognome del dipendente</p>
+            <input type="text" name="cognome" placeholder="Cognome" required><br>
+            <p>Inserisci il codice fiscale del dipendente</p>
+            <input type="text" name="cf" placeholder="Codice Fiscale" style="text-transform: uppercase;" required><br>
+            <p>Inserisci la data di nascita del dipendente</p>
+            <input type="date" name="data_di_nascita" placeholder="Data di nascita" required><br>
+            <input type="submit" value="Inserisci Dipendente">
+        </form>
+        <br>
+        <a href="home.php">Torna alla Home</a>
     </body>
 </html>
