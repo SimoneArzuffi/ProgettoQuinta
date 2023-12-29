85902460cd8567581c74333b72ebe1b7fafcdaf4
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2023-12-29 11:07:59 +0000

    primo css

--- inserisciMalattia.php
+++ inserisciMalattia.php
@@ -2,49 +2,99 @@
 <html>
     <?php
         session_start();
-        if(isset($_POST['nome']) && isset($_POST['cognome']) && isset($_POST['numero_malattia']) && isset($_POST['data_inizio']) && isset($_POST['data_fine']) && isset($_POST['giorni'])){
+        if(isset($_POST['nome']) && isset($_POST['cognome']) && isset($_POST['data_inizio']) && isset($_POST['data_fine']) && isset($_POST['giorni'])){
             //controllo se data inizio è minore di data fine
             if($_POST['data_inizio'] > $_POST['data_fine']){
                 echo "data inizio maggiore di data fine";
             }else{
-                //controlla se il numero di giorni inserito corrisponde alla differenza tra data inizio e data fine maggioarata di 1
+                //controlla se il numero di giorni inserito corrisponde alla differenza tra data inizio e data fine
                 $data_inizio = new DateTime($_POST['data_inizio']);
                 $data_fine = new DateTime($_POST['data_fine']);
                 $differenza = $data_inizio->diff($data_fine);
                 if($differenza->days != $_POST['giorni'] - 1){
                     echo "numero giorni non corrispondente alla differenza tra la data di inizio e la data di fine";
                 }else{
-                    //controlla se il numero di malattia è numerico e di 9 cifre
-                    if(!is_numeric($_POST['numero_malattia']) || strlen($_POST['numero_malattia']) != 9){
-                        echo "numero malattia non valido";
-                    }else{
-                        $_SESSION['POST'] = $_POST;
-                        header('Location: /www/addMalattia.php');
-                    }
+                    $_SESSION['POST'] = $_POST;
+                    header('Location: /www/addFerie.php');
                 }
             }
         }
     ?>
     <head>
-        <title>
-            inserisci malattia
-        </title>
+        <title>Inserisci Ferie</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #f4f4f4;
+            }
+
+            form {
+                width: 300px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+
+            p {
+                margin-top: 10px;
+                margin-bottom: 5px;
+                font-weight: bold;
+            }
+
+            input[type="text"],
+            input[type="date"],
+            input[type="number"],
+            input[type="submit"] {
+                width: calc(100% - 10px);
+                padding: 10px;
+                margin-bottom: 15px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+
+            input[type="submit"] {
+                background-color: #28a745;
+                color: #fff;
+                cursor: pointer;
+            }
+
+            input[type="submit"]:hover {
+                background-color: #218838;
+            }
+
+            a {
+                display: block;
+                margin-top: 10px;
+                text-align: center;
+                text-decoration: none;
+                color: #007bff;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
     </head>
     <body>
-        <form action="inserisciMalattia.php" method="POST">
-            <p>inserisci il nome del dipendente</p>
-            <input type="nome" name="nome" placeholder="nome" required><br><br>
-            <p>inserisci il cognome del dipendente</p>
-            <input type="cognome" name="cognome" placeholder="cognome" required><br><br>
-            <p>inserisci il numero di malattia</p>
-            <input type="text" minlength="9" maxlength="9" name="numero_malattia" placeholder="numero malattia" required><br><br>
-            <p>inserisci la data di inizio</p>
-            <input type="date" name="data_inizio" placeholder="data inizio" required><br><br>
-            <p>inserisci la data di fine</p>
-            <input type="date" name="data_fine" placeholder="data fine" required><br><br>
-            <p>inserisci il numero di giorni</p>
-            <input type="number" min="0" name="giorni" placeholder="giorni" required><br><br>
-            <input type="submit" value="inserisci malattia">
-        </form><br>
-        <a href="home.php">torna alla home</a>
+        <form action="inserisciFerie.php" method="POST">
+            <p>Inserisci il nome del dipendente</p>
+            <input type="text" name="nome" placeholder="Nome" required><br>
+            <p>Inserisci il cognome del dipendente</p>
+            <input type="text" name="cognome" placeholder="Cognome" required><br>
+            <p>Inserisci la data di inizio</p>
+            <input type="date" name="data_inizio" placeholder="Data inizio" required><br>
+            <p>Inserisci la data di fine</p>
+            <input type="date" name="data_fine" placeholder="Data fine" required><br>
+            <p>Inserisci il numero di giorni</p>
+            <input type="number" min="0" name="giorni" placeholder="Giorni" required><br>
+            <input type="submit" value="Inserisci Ferie">
+        </form>
+        <br>
+        <a href="home.php">Torna alla Home</a>
+    </body>
 </html>
--- inserisciPermessi.php
+++ inserisciPermessi.php
@@ -2,31 +2,85 @@
 <html>
     <?php
         session_start();
-        //controllo se i dati sono stati inseriti e basta
         if(isset($_POST['nome']) && isset($_POST['cognome']) && isset($_POST['data']) && isset($_POST['ora_di_inizio']) && isset($_POST['ora_di_fine'])){
             $_SESSION['POST'] = $_POST;
             header("Location: addPermessi.php");
         }
     ?>
     <head>
-        <title>
-            inserisci permessi
-        </title>
+        <title>Inserisci Permessi</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #f4f4f4;
+            }
+
+            form {
+                width: 300px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+
+            p {
+                margin-top: 10px;
+                margin-bottom: 5px;
+            }
+
+            input[type="text"],
+            input[type="date"],
+            input[type="time"],
+            input[type="submit"] {
+                width: calc(100% - 10px);
+                padding: 10px;
+                margin-bottom: 15px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+
+            input[type="submit"] {
+                background-color: #28a745;
+                color: #fff;
+                cursor: pointer;
+            }
+
+            input[type="submit"]:hover {
+                background-color: #218838;
+            }
+
+            a {
+                display: block;
+                margin-top: 10px;
+                text-align: center;
+                text-decoration: none;
+                color: #007bff;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
     </head>
     <body>
         <form action="inserisciPermessi.php" method="POST">
-            <p>inserisci il nome del dipendente</p>
-            <input type="nome" name="nome" placeholder="nome" required><br><br>
-            <p>inserisci il cognome del dipendente</p>
-            <input type="cognome" name="cognome" placeholder="cognome" required><br><br>
-            <p>inserisci la data della richista di permesso</p>
-            <input type="date" name="data" placeholder="data" required><br><br>
-            <p>inserisci l'ora di inizio</p>
-            <input type="time" min="0" name="ora_di_inizio" placeholder="ora di inizio" required><br><br>
-            <p>inserisci l'ora di fine</p>
-            <input type="time" min="0" name="ora_di_fine" placeholder="ora di fine" required><br><br>
-            <input type="submit" value="inserisci permesso">
-        </form><br>
-        <a href="home.php">torna alla home</a>
+            <p>Inserisci il nome del dipendente</p>
+            <input type="text" name="nome" placeholder="Nome" required><br>
+            <p>Inserisci il cognome del dipendente</p>
+            <input type="text" name="cognome" placeholder="Cognome" required><br>
+            <p>Inserisci la data della richiesta di permesso</p>
+            <input type="date" name="data" required><br>
+            <p>Inserisci l'ora di inizio</p>
+            <input type="time" name="ora_di_inizio" required><br>
+            <p>Inserisci l'ora di fine</p>
+            <input type="time" name="ora_di_fine" required><br>
+            <input type="submit" value="Inserisci Permesso">
+        </form>
+        <br>
+        <a href="home.php">Torna alla Home</a>
     </body>
 </html>
