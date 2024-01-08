f7f62d332b2c8170d34ecd45d3493aa4b0f2e07f
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2024-01-08 12:46:03 +0000

    da finire

--- mostraDipendenti.php
+++ mostraDipendenti.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
+    <?php
+        session_start();
+        include "connection.php"; 
+    ?>
     <head>
-        <title>mmostra dipendenti</title>
+        <title>mostra dipendenti</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
@@ -40,10 +44,41 @@
         </style>
     </head>
     <body>
+        <h1> WORK IN PROGRESS </h1>
         <!--
             1. crea pulsante che estrae e mette i dipendenti in una tabella
             2. crea pulsante che estrae solo il dipendente selezionato e lo mette in una tabella
         -->
-
+        <div class="button-container">
+            <form action="mostraDipendenti.php" method="POST"><input type="submit" value="Visualizza Dipendenti"></form>
+        </div>
     </body>
+    <?php
+        if (isset($_POST['submit'])) {
+            $query = "SELECT * FROM dipendente";
+            $result = mysqli_query($connessione, $query);
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Nome</th>";
+            echo "<th>Cognome</th>";
+            echo "<th>CF</th>";
+            echo "<th>Data di nascita</th>";
+            echo "<th>Ore di permesso</th>";
+            echo "<th>Giorni di ferie</th>";
+            echo "<th>Giorni di malattia</th>";
+            echo "</tr>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['nome'] . "</td>";
+                echo "<td>" . $row['cognome'] . "</td>";
+                echo "<td>" . $row['cf'] . "</td>";
+                echo "<td>" . $row['data_di_nascita'] . "</td>";
+                echo "<td>" . $row['ore_di_permesso'] . "</td>";
+                echo "<td>" . $row['giorni_di_ferie'] . "</td>";
+                echo "<td>" . $row['giorni_di_malattia'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+    ?>
 </html>
