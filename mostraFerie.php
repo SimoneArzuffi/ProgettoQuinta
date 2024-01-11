83dc63eafb7ca8fff9a6554f1dc2a0d26edc2a7e
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2024-01-11 10:26:52 +0000

    aggiunto filtro per nome e cognome

--- mostraFerie.php
+++ mostraFerie.php
@@ -86,34 +86,55 @@
         </style>
     </head>
     <body>
-        <form>
-            <input type="submit" value="Mostra ferie">
+        <form action="mostraFerie.php" method="post">
+            <input type="text" name="nome" placeholder="nome">
+            <input type="text" name="cognome" placeholder="cognome">
+            <input type="submit" value="cerca">
+            <input type="submit" name="mostraTutti" value="Mostra ferie">
         </form>
         <a href="home.php">Torna alla home</a>
         <?php
-            $query = "SELECT nome, cognome, data_inizio, data_fine, giorni FROM dipendente, ferie WHERE dipendente.id = ferie.id_dipendente";
-            $result = mysqli_query($connessione, $query);
-            if (mysqli_num_rows($result) > 0) {
-                echo "<table>";
-                echo "<tr>";
-                echo "<th>Id</th>";
-                echo "<th>Id dipendente</th>";
-                echo "<th>Data inizio</th>";
-                echo "<th>Data fine</th>";
-                echo "<th>Giorni</th>";
-                echo "</tr>";
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['nome'] . "</td>";
-                    echo "<td>" . $row['cognome'] . "</td>";
-                    echo "<td>" . $row['data_inizio'] . "</td>";
-                    echo "<td>" . $row['data_fine'] . "</td>";
-                    echo "<td>" . $row['giorni'] . "</td>";
-                    echo "</tr>";
+            if (isset($_POST['mostraTutti'])) {
+                $query = "SELECT nome, cognome, data_inizio, data_fine, giorni FROM dipendente, ferie WHERE dipendente.id = ferie.id_dipendente";
+                $result = mysqli_query($connessione, $query);
+            }elseif(isset($_POST["nome"]) && isset($_POST["cognome"])){
+                if($_POST["nome"] != "" && $_POST["cognome"] != ""){
+                    $query = "SELECT nome, cognome, data_inizio, data_fine, giorni FROM dipendente, ferie WHERE dipendente.id = ferie.id_dipendente AND nome = '".$_POST["nome"]."' AND cognome = '".$_POST["cognome"]."'";
+                    $result = mysqli_query($connessione, $query);
+                }elseif($_POST["nome"] != "" && $_POST["cognome"] == ""){
+                    $query = "SELECT nome, cognome, data_inizio, data_fine, giorni FROM dipendente, ferie WHERE dipendente.id = ferie.id_dipendente AND nome = '".$_POST["nome"]."'";
+                    $result = mysqli_query($connessione, $query);
+                }elseif($_POST["nome"] == "" && $_POST["cognome"] != ""){
+                    $query = "SELECT nome, cognome, data_inizio, data_fine, giorni FROM dipendente, ferie WHERE dipendente.id = ferie.id_dipendente AND cognome = '".$_POST["cognome"]."'";
+                    $result = mysqli_query($connessione, $query);
                 }
-                echo "</table>";
-            } else {
-                echo "<p>Nessun risultato trovato.</p>";
+            }
+            if(isset($result)){
+                if (mysqli_num_rows($result) > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>Id</th>";
+                    echo "<th>Id dipendente</th>";
+                    echo "<th>Data inizio</th>";
+                    echo "<th>Data fine</th>";
+                    echo "<th>Giorni</th>";
+                    echo "</tr>";
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo "<td>" . $row['nome'] . "</td>";
+                        echo "<td>" . $row['cognome'] . "</td>";
+                        //cambia formato data da yyyy-mm-dd a dd/mm/yyyy
+                        $data_inizio = date("d/m/Y", strtotime($row['data_inizio']));
+                        echo "<td>" . $data_inizio . "</td>";
+                        $data_fine = date("d/m/Y", strtotime($row['data_fine']));
+                        echo "<td>" . $data_fine . "</td>";
+                        echo "<td>" . $row['giorni'] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<p>Nessun risultato trovato.</p>";
+                }
             }
         ?>
         
