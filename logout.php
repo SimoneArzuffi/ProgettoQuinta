4a2e3634a61a1f8c23d0b1cdd61ffdc2ef43acfe
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2024-01-18 10:56:51 +0000

    aggiunto $connessione->close();

--- logout.php
+++ logout.php
@@ -8,8 +8,10 @@
     <body>
         <?php
             session_start();
+            include "connection.php";
             $_SESSION = array();
             session_destroy();
+            $connessione->close();
             header('Location: /www/login.php');
         ?>
     </body>
