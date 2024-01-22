1f60a8f9ce2a4abb6ae683be6b0c9f419ac6d89a
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2024-01-22 12:26:52 +0000

    aggiunto filtro di ricerca permessi

--- mostraPermessi.php
+++ mostraPermessi.php
@@ -10,11 +10,14 @@
     </head>
     <body>
         <form action="mostraPermessi.php" method="post">
+            <input type="text" name="nome" placeholder="Nome">
+            <input type="text" name="cognome" placeholder="Cognome">
+            <input type="submit" name="cerca" value="cerca">
             <input type="submit" name="mostraTutti" value="Mostra permessi">
         </form>
         <?php
             if (isset($_POST["mostraTutti"])) {
-                $query = "SELECT * FROM permessi";
+                $query = "SELECT nome, cognome, ora_inizio, ora_fine FROM dipendente, permesso WHERE dipendente.id = permesso.id_dipendente";
                 $result = mysqli_query($connessione, $query);
                 if (mysqli_num_rows($result) > 0) {
                     echo "<table>";
@@ -35,6 +38,79 @@
                     echo "</table>";
                 } else {
                     echo "Nessun permesso trovato";
+                }
+            }elseif (isset($_POST["cerca"])) {
+                if($_POST["nome"] != "" && $_POST["cognome"] != ""){
+                    $query = "SELECT nome, cognome, ora_inizio, ora_fine FROM dipendente, permesso WHERE dipendente.id = permesso.id_dipendente AND nome = '" . $_POST["nome"] . "' AND cognome = '" . $_POST["cognome"] . "'";
+                    $result = mysqli_query($connessione, $query);
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>Nome</th>";
+                        echo "<th>Cognome</th>";
+                        echo "<th>Ora di inizio</th>";
+                        echo "<th>Ora di fine</th>";
+                        echo "</tr>";
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["nome"] . "</td>";
+                            echo "<td>" . $row["cognome"] . "</td>";
+                            echo "<td>" . $row["ora_inizio"] . "</td>";
+                            echo "<td>" . $row["ora_fine"] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "Nessun permesso trovato";
+                    }
+                }elseif($_POST["nome"] != "" && $_POST["cognome"] == ""){
+                    $query = "SELECT nome, cognome, ora_inizio, ora_fine FROM dipendente, permesso WHERE dipendente.id = permesso.id_dipendente AND nome = '" . $_POST["nome"] . "'";
+                    $result = mysqli_query($connessione, $query);
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>Nome</th>";
+                        echo "<th>Cognome</th>";
+                        echo "<th>Ora di inizio</th>";
+                        echo "<th>Ora di fine</th>";
+                        echo "</tr>";
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["nome"] . "</td>";
+                            echo "<td>" . $row["cognome"] . "</td>";
+                            echo "<td>" . $row["ora_inizio"] . "</td>";
+                            echo "<td>" . $row["ora_fine"] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "Nessun permesso trovato";
+                    }
+                }elseif($_POST["nome"] == "" && $_POST["cognome"] != ""){
+                    $query = "SELECT nome, cognome, ora_inizio, ora_fine FROM dipendente, permesso WHERE dipendente.id = permesso.id_dipendente AND cognome = '" . $_POST["cognome"] . "'";
+                    $result = mysqli_query($connessione, $query);
+                    if (mysqli_num_rows($result) > 0) {
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>Nome</th>";
+                        echo "<th>Cognome</th>";
+                        echo "<th>Ora di inizio</th>";
+                        echo "<th>Ora di fine</th>";
+                        echo "</tr>";
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["nome"] . "</td>";
+                            echo "<td>" . $row["cognome"] . "</td>";
+                            echo "<td>" . $row["ora_inizio"] . "</td>";
+                            echo "<td>" . $row["ora_fine"] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "Nessun permesso trovato";
+                    }
+                }else{
+                    echo "Inserisci almeno un campo";
                 }
             }
         ?>
