0575d03d10170a97df5c9cdc34b48e0cbaa403e2
SimoneArzuffi <arzuffi.simone.studente@itispaleocapa.it>
2024-04-03 16:20:03 +0000

    aggiunto inserisci azienda

---
+++ addAzienda.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    include "connection.php";
+    $sql = "SELECT nome FROM azienda";
+    
+    $result = $connessione->query($sql);
+
+    $res = $result->fetch_all();
+
+    foreach($res as $r){
+        if($r == $_POST['nome']){
+            header("Location: inserisciAzienda.php");
+        }
+    }
+
+    $sql = "INSERT INTO azienda (nome) VALUES (" . $_POST['nome'] . ");";
+
+    if(!$risultati=$connessione->query($la_query)) {
+        echo("Errore nell'esecuzione della query: ".$connessione->error.".");
+        exit();
+    }
+
+    $connessione -> close();
+
+    header("Location: inserisciAzienda.php");
+
+?>
---
+++ inserisciAzienda.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        session_start();
+        if(isset($_POST["nome"])){
+            $_SESSION['POST'] = $_POST;
+            header('Location: addAzienda.php');
+        }
+    ?>
+    <head>
+        <title>Inserisci Dipendente</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #f4f4f4;
+            }
+
+            form {
+                width: 300px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+
+            p {
+                margin-top: 10px;
+                margin-bottom: 5px;
+            }
+
+            input[type="text"],
+            input[type="date"],
+            input[type="submit"] {
+                width: calc(100% - 10px);
+                padding: 10px;
+                margin-bottom: 15px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                font-size: 16px;
+            }
+
+            input[type="submit"] {
+                background-color: #4caf50;
+                color: #fff;
+                cursor: pointer;
+            }
+
+            input[type="submit"]:hover {
+                background-color: #0056b3;
+            }
+
+            a {
+                display: block;
+                margin-top: 10px;
+                text-align: center;
+                text-decoration: none;
+                color: #007bff;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="inserisciAzienda.php" method="POST">
+            <p>Inserisci il nome dell'azienda</p>
+            <input type="text" name="nome" placeholder="Nome" required><br>
+            <input type="submit" value="Inserisci Dipendente">
+        </form>
+        <br>
+        <a href="index.php">Torna alla Home</a>
+    </body>
+</html>
