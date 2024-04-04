041f93bb5ddeba03fd8068599696b2135ef65509
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-04-04 09:48:53 +0000

    sistemato errore

--- addAzienda.php
+++ addAzienda.php
@@ -3,22 +3,27 @@
     include "connection.php";
     $sql = "SELECT nome FROM azienda";
     
-    $result = $connessione->query($sql);
+    $post = $_SESSION['POST']; // Assicurati che questa riga sia corretta. Potresti voler utilizzare $_POST direttamente se stai cercando di accedere ai dati inviati da un form.
 
-    $res = $result->fetch_all();
-
-    foreach($res as $r){
-        if($r == $_POST['nome']){
-            header("Location: inserisciAzienda.php");
-        }
-    }
-
-    $sql = "INSERT INTO azienda (nome) VALUES ('" . $_POST['nome'] . "');";
-
-    if(!$risultati=$connessione->query($sql)) {
-        echo("Errore nell'esecuzione della query: ".$connessione->error.".");
+    // Utilizzo di prepared statement per l'inserimento
+    $sql = "INSERT INTO azienda (nome) VALUES (?);";
+    $stmt = $connessione->prepare($sql);
+    
+    // "s" indica che il parametro è una stringa
+    $stmt->bind_param("s", $post['nome']);
+    
+    if(!$stmt->execute()) {
+        echo("Errore nell'esecuzione della query: " . $stmt->error);
+        exit();
+    } else {
+        // Redirect se l'inserimento è riuscito
+        header("Location: inserisciAzienda.php");
         exit();
     }
+    
+    // Non è necessario chiudere la connessione esplicitamente in script come questo, 
+    // poiché verrà chiusa automaticamente alla fine dello script.
+    
 
     $connessione -> close();
 
