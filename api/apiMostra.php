09c4555ae9340958a5702756e63e413241e621f7
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-04-08 10:32:25 +0000

    aggiunte migliorie

--- api/apiMostra.php
+++ api/apiMostra.php
@@ -9,16 +9,16 @@
     // Controllare se i parametri sono vuoti
     if ($nome == "" && $cognome == "") {
         // Se sono vuoti, eseguire una query per ottenere tutti i dipendenti
-        $sql = "SELECT * FROM dipendente";
+        $sql = "SELECT * FROM dipendente INNER JOIN azienda ON dipendente.id_azienda = azienda.id;";
     } else if($nome == "" && $cognome != ""){
         // Se il nome è vuoto e il cognome è pieno, eseguire una query per ottenere i dipendenti con quel cognome
-        $sql = "SELECT * FROM dipendente WHERE cognome = '$cognome'";
+        $sql = "SELECT * FROM dipendente INNER JOIN azienda ON dipendente.id_azienda = azienda.id WHERE cognome = '$cognome'";
     } else if($nome != "" && $cognome == ""){
         // Se il nome è pieno e il cognome è vuoto, eseguire una query per ottenere i dipendenti con quel nome
-        $sql = "SELECT * FROM dipendente WHERE nome = '$nome'";
+        $sql = "SELECT * FROM dipendente INNER JOIN azienda ON dipendente.id_azienda = azienda.id WHERE nome = '$nome'";
     } else {
         // Se entrambi i parametri sono pieni, eseguire una query per ottenere i dipendenti con quel nome e cognome
-        $sql = "SELECT * FROM dipendente WHERE nome = '$nome' AND cognome = '$cognome'";
+        $sql = "SELECT * FROM dipendente INNER JOIN azienda ON dipendente.id_azienda = azienda.id WHERE nome = '$nome' AND cognome = '$cognome'";
     }
 
     // Eseguire la query e ottenere il risultato
@@ -28,9 +28,10 @@
     $res = $result->fetch_all();
 
     // Iterare attraverso ogni riga del risultato e stampare le informazioni desiderate
+    echo '<div class="user">Nome Cognome Azienda<';
     foreach ($res as $r) {
         // Stampare una riga di HTML per ogni risultato, con il nome e cognome e un pulsante di eliminazione usa div 
-        echo '<div class="user">' . $r[1] . " " . $r[2] . ' <button class="delete-btn" data-id="' . $r[0] . '">Delete</button></div>';
+        echo '<div class="user">' . $r[1] . " " . $r[2] . " " . $r[7] . ' <button class="delete-btn" data-id="' . $r[0] . '">Delete</button></div>';
     }
 
     // Chiudere la cone$connessione al database
