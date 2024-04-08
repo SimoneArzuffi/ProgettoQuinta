e65df48bf29310256b2d27bb55e7c23202701e67
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-04-08 10:24:24 +0000

    sistemato errore

--- addDipendente.php
+++ addDipendente.php
@@ -14,7 +14,7 @@
         {
             $cf = strtoupper($post['cf']);
             //crea query per inserire il gestore, inizializza ore di permesso, giorni di ferie e giorni di malattia a 0
-            $la_query = "INSERT INTO dipendente (nome, cognome, cf, data_nascita, id_azienda) VALUES ('" . $post['nome'] . "' , '" . $post['cognome'] . "', '" . $cf . "', '" . $post['data_di_nascita']. ", '" . $post['azienda'] . "')";
+            $la_query = "INSERT INTO dipendente (nome, cognome, cf, data_di_nascita, id_azienda) VALUES ('" . $post['nome'] . "' , '" . $post['cognome'] . "', '" . $cf . "', '" . $post['data_di_nascita']. "', '" . $post['azienda'] . "')";
            if(!$risultati=$connessione->query($la_query)) {
                 echo("Errore nell'esecuzione della query: ".$connessione->error.".");
                 exit();
