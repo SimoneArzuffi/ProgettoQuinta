b3cb3f2dc16dee0c7fce39d851e5f2240a30d25b
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-06-03 11:01:18 +0000

    sistemato errore

--- mostraDipendenti.php
+++ mostraDipendenti.php
@@ -4,18 +4,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
-        <script src="js/apiMostraDipendente.js"></script>
+        <link rel="stylesheet" type="text/css" href="file_css/mostra.css">
     </head>
 
     <body>
-    <form>
-        <Inserisci campi per ricerca dipendente>
-        <input type="text" id="nome" placeholder="Nome">
-        <input type="text" id="cognome" placeholder="Cognome">
-        <input type="button" id="button" value="mostra">
-    </form>
-    <div id="tabella"></div>
-    <a href="index.php">Torna alla home</a>
-</body>
+        <form>
+            <Inserisci campi per ricerca dipendente>
+            <input type="text" id="nome" placeholder="Nome">
+            <input type="text" id="cognome" placeholder="Cognome">
+            <input type="button" id="button" value="mostra">
+        </form>
+        <div id="tabella"></div>
+        
+        <a href="index.php">Torna alla home</a>
+
+        <script src="js/apiMostraDipendente.js"></script>
+    </body>
 </html>
